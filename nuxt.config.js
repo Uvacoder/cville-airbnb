1dbcaa9b4db93540a1fb47f53bfb8afe5bb1b004
josh deltener <hecktarzuli@gmail.com>
2020-10-31 08:58:41 -0500

    10 - metas

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,15 @@
 export default {
     components: true,
+    head: {
+        titleTemplate: "Mastering Nuxt: %s",
+        htmlAttrs: {
+            lang: "en"
+        },
+        bodyAttrs:{
+            class: ["my-style"]
+        },
+        meta: [{
+            charset: "utf-8",
+        }]
+    }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,16 @@
 import homes from '~/data/homes'
 
 export default {
+    head(){
+        return {
+            title: 'Homepage',
+            meta: [{
+                name: 'description',
+                content: 'This is a homepage!',
+                hid: 'description'
+            }]
+        }
+    },
     data(){
         return {
             homes: homes.slice(0,3)
