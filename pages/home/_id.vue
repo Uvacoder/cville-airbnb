a72cfc8ff9383931c1bbbb494e56404ff0746244
josh deltener <hecktarzuli@gmail.com>
2020-11-01 08:01:38 -0600

    11 - routing

---
+++ pages/home/_id.vue
@@ -0,0 +1,17 @@
+<template>
+<div>{{ home.title }}</div>
+</template>
+<script>
+import homes from '~/data/homes'
+export default {
+    data(){
+        return {
+            home: {}
+        }
+    },
+    created(){
+        const home = homes.find((home) => home.objectID == this.$route.params.id)
+        this.home = home
+    }
+}
+</script>
