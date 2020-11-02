61dad2d33c7ffd60df96c14052b6322644d3fd49
josh deltener <hecktarzuli@gmail.com>
2020-11-01 21:38:14 -0600

    12 - property basics

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,9 +1,23 @@
 <template>
-<div>{{ home.title }}</div>
+<div>
+    <div style="display:flex;">
+        <img v-for="image in home.images" :key="image" :src="image" width="200" height="150"/>
+    </div>
+    {{ home.title }}<br/>
+    ${{ home.pricePerNight }} / night<br/>
+    <img src="/images/marker.svg" width="20" height="20"/>{{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}<br/>
+    <img src="/images/star.svg" width="20" height="20"/>{{ home.reviewValue}} <br/>
+    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath<br/>
+    </div>
 </template>
 <script>
 import homes from '~/data/homes'
 export default {
+    head(){
+        return {
+            title: this.home.title
+        }
+    },
     data(){
         return {
             home: {}
