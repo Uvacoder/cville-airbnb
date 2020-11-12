d2eed059e7be3ea8c92080b3106abdacda674ff6
josh deltener <hecktarzuli@gmail.com>
2020-11-11 21:27:30 -0600

    where did description go??

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -8,6 +8,7 @@
     <img src="/images/marker.svg" width="20" height="20"/>{{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}<br/>
     <img src="/images/star.svg" width="20" height="20"/>{{ home.reviewValue}} <br/>
     {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath<br/>
+    {{ home.description }}
     </div>
 </template>
 <script>
@@ -15,7 +16,7 @@ import homes from '~/data/homes'
 export default {
     head(){
         return {
-            title: this.home.title
+            title: this.home.title,
         }
     },
     data(){
