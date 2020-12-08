368644aee8d971a1e8d518a953a49abdd188a168
josh deltener <hecktarzuli@gmail.com>
2020-12-07 21:36:49 -0600

    30 - refactoring for speed

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -33,20 +33,20 @@ export default {
     mounted(){
         this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng)
     },
-    async asyncData({ params, $dataApi, error }){        
-        const homeResponse = await $dataApi.getHome(params.id)             
-        if(!homeResponse.ok) return error({ statusCode: homeResponse.status, message: homeResponse.statusText})
+    async asyncData({ params, $dataApi, error }){    
+        const responses = await Promise.all([
+            $dataApi.getHome(params.id),
+            $dataApi.getReviewsByHomeId(params.id),
+            $dataApi.getUserByHomeId(params.id),
+        ])
 
-        const reviewResponse = await $dataApi.getReviewsByHomeId(params.id)        
-        if(!reviewResponse.ok) return error({ statusCode: reviewResponse.status, message: reviewResponse.statusText})
-
-        const userResponse = await $dataApi.getUserByHomeId(params.id)        
-        if(!userResponse.ok) return error({ statusCode: userResponse.status, message: userResponse.statusText})
+        const badResponse = responses.find((response) => !response.ok)
+        if(badResponse) return error({ statusCode: badResponse.status, message: badResponse.statusText})
 
         return {
-            home: homeResponse.json,
-            reviews: reviewResponse.json.hits,
-            user: userResponse.json.hits[0]
+            home: responses[0].json,
+            reviews: responses[1].json.hits,
+            user: responses[2].json.hits[0]
         }
     },
     methods:{
