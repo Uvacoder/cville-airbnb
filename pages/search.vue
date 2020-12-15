eb6c2ff65792f68450c3c2aa90e031ca3e38cde1
josh deltener <hecktarzuli@gmail.com>
2020-12-14 21:30:20 -0600

    34 - geo search results

--- pages/search.vue
+++ pages/search.vue
@@ -1,3 +1,18 @@
-<template
-  ><div>{{ $route.query }}</div></template
->
+<template>
+<div>{{ lat}} / {{ lng }} / {{ label }}<br/>
+<div v-for="home in homes" :key="home.objectID">{{ home.title}}<br/></div>
+</div></template>
+<script>
+export default {
+  watchQuery:['lat'],  
+  async asyncData({ query, $dataApi }){
+    const data = await $dataApi.getHomesByLocation(query.lat, query.lng)
+    return {
+      homes: data.json.hits,
+      label: query.label,
+      lat: query.lat,
+      lng: query.lng,
+    }
+  }
+}
+</script>
--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -9,6 +9,7 @@ export default function(context, inject){
         getHome,
         getReviewsByHomeId,
         getUserByHomeId,
+        getHomesByLocation,
     })
 
     async function getHome(homeId){
@@ -42,6 +43,23 @@ export default function(context, inject){
                 method: 'POST',
                 body: JSON.stringify({
                     filters: `homeId:${homeId}`,                    
+                    attributesToHighlight: [],
+                })
+            }))
+        } catch(error){
+            return getErrorResponse(error)
+        }
+    }
+
+    async function getHomesByLocation(lat, lng, radiusInMeters = 1500){
+        try {
+            return unWrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/query`, {
+                headers,
+                method: 'POST',
+                body: JSON.stringify({
+                    aroundLatLng: `${lat},${lng}`,
+                    aroundRadius: radiusInMeters,
+                    hitsPerPage: 10,
                     attributesToHighlight: [],
                 })
             }))
