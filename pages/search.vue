6f9d3b1cec871f6cb06a1be3bb8cb2a661c9383e
Josh Deltener <hecktarzuli@gmail.com>
2021-01-04 21:09:41 -0600

    40 - map styles and marker effects

--- pages/search.vue
+++ pages/search.vue
@@ -3,7 +3,7 @@
 <div style="height:800px;width:800px;float:right;" ref="map"></div>
   <div v-if="homes.length > 0"> 
     <nuxt-link v-for="home in homes" :key="home.objectID" :to="`/home/${home.objectID}`">
-    <HomeRow :home="home"/>
+    <HomeRow :home="home" @mouseover.native="highlightMarker(home.objectID, true)" @mouseout.native="highlightMarker(home.objectID, false)"/>
     </nuxt-link> 
   </div>
   <div v-else>No results found</div>
@@ -20,6 +20,9 @@ export default {
     this.updateMap()
   },
   methods:{
+    highlightMarker(homeId, isHighlighted){
+      document.getElementsByClassName(`home-${homeId}`)[0]?.classList?.toggle('marker-highlight', isHighlighted)
+    },
     updateMap(){
       this.$maps.showMap(this.$refs.map, this.lat, this.lng, this.getHomeMarkers())
     },
@@ -28,6 +31,7 @@ export default {
         return {
           ...home._geoloc,
           pricePerNight: home.pricePerNight,
+          id: home.objectID,
         }
       })
     }
@@ -60,5 +64,10 @@ export default {
   font-weight: bold;
   border-radius: 20px;
   padding: 5px 8px;
+}
+.marker-highlight {
+  color: white !important;
+  background-color: black;
+  border-color:black;
 }
 </style>
