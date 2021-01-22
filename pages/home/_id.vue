61230c7f9e7d7e55cf4f55706e6dddf72c11de94
Josh Deltener <hecktarzuli@gmail.com>
2021-01-21 20:05:46 -0600

    48 - styling the property page - part 2

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -2,29 +2,11 @@
 <div class="app-container">
     <PropertyGallery :images="home.images"/>
     <PropertyDetails :home="home"/>
-    
-    <div style="display:flex;">
-        <img v-for="image in home.images" :key="image" :src="image" width="200" height="150"/>
-    </div>
-    {{ home.title }}<br/>
-    ${{ home.pricePerNight }} / night<br/>
-    <img src="/images/marker.svg" width="20" height="20"/>{{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}<br/>
-    <img src="/images/star.svg" width="20" height="20"/>{{ home.reviewValue}} <br/>
-    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath<br/>
-    {{ home.description }}
-    <div style="height:800px;width:800px" ref="map"></div>
-    <div v-for="review in reviews" :key="review.objectID">
-        <img :src="review.reviewer.image"/><br/>
-        {{ review.reviewer.name }}<br/>
-        {{ formatDate(review.date) }} <br/>
-        <short-text :text="review.comment" :target="150"/><br/>
-    </div>
-    <img :src="user.image"/><br/>
-    {{ user.name }} <br/>
-    {{ formatDate(user.joined) }} <br/>
-    {{ user.reviewCount }} <br/>
-    {{ user.description }} <br/>
-    </div>
+    <PropertyDescription :home="home"/>
+    <PropertyMap :home="home"/>
+    <PropertyReviews :reviews="reviews"/>
+    <PropertyHost :user="user"/>    
+</div>
 </template>
 <script>
 export default {
@@ -33,9 +15,6 @@ export default {
             title: this.home.title,                   
         }
     },   
-    mounted(){
-        this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng)
-    },
     async asyncData({ params, $dataApi, error }){    
         const responses = await Promise.all([
             $dataApi.getHome(params.id),
@@ -52,11 +31,5 @@ export default {
             user: responses[2].json.hits[0]
         }
     },
-    methods:{
-        formatDate(dateStr){
-            const date = new Date(dateStr)
-            return date.toLocaleDateString(undefined, { month: 'long', year: 'numeric' })
-        }
-    }
 }
 </script>
