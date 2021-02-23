4b325022474f6d28e3f011fddc8e20a9f3d8b52b
Josh Deltener <hecktarzuli@gmail.com>
2021-02-22 19:16:14 -0600

    58 - building an API firewall part 2

--- modules/auth.js
+++ modules/auth.js
@@ -1,4 +1,5 @@
 import cookie from 'cookie'
+import {OAuth2Client} from 'google-auth-library'
 
 export default function() {
     const authConfig = this.options.publicRuntimeConfig.auth
@@ -7,10 +8,39 @@ export default function() {
         app.use('/api', handler)
     })
 
-    function handler(req, res, next){
+    async function handler(req, res, next){
         const idToken = cookie.parse(req.headers.cookie)[authConfig.cookieName]
+        if(!idToken) return rejectHit(res)
+
         console.log(req.originalUrl)
         console.log(idToken)
+        const ticket = await getUser(idToken)
+        if(!ticket) return rejectHit(res)
+        console.log(ticket)
+        req.identity = {
+            id: ticket.sub,
+            email: ticket.email,
+            name: ticket.name,
+            image: ticket.picture,
+        }
         next()
+    }
+
+    async function getUser(idToken){
+        const client = new OAuth2Client(authConfig.clientId)
+        try {
+            const ticket = await client.verifyIdToken({
+                idToken,
+                audience: authConfig.clientId,
+            })
+            return ticket.getPayload()
+        }
+        catch(error){
+            console.error(error)
+        }        
+    }
+    function rejectHit(res){
+        res.statusCode = 401
+        res.end()
     }
 }
