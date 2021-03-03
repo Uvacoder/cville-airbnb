d30b76dbb9927bcb799077207d3908eafe0b3e1b
Josh Deltener <hecktarzuli@gmail.com>
2021-03-02 23:40:17 -0600

    64 - protecting the admin section

--- pages/admin.vue
+++ pages/admin.vue
@@ -4,3 +4,15 @@
         <nuxt-child></nuxt-child>
     </div>
 </template>
+<script>
+import Cookie from 'js-cookie'
+
+export default {
+    asyncData({ $config, redirect }){
+        if(!Cookie.get($config.auth.cookieName)){
+            redirect('/no-access/')
+            return
+        }
+    }
+}
+</script>
