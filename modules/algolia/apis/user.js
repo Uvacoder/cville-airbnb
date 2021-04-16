03b194d1f8f1163b0c1ef417d62ede24a147b02c
Josh Deltener <hecktarzuli@gmail.com>
2021-04-15 20:01:19 -0500

    76 - assigning users to homes

--- modules/algolia/apis/user.js
+++ modules/algolia/apis/user.js
@@ -5,6 +5,11 @@ import { unWrap, getErrorResponse } from '../../../utils/fetchUtils'
 export default (algoliaConfig) => {
     const headers = getHeaders(algoliaConfig)
     return {
+        assignHome: async function(identity, homeId){
+            const payload = (await this.getById(identity)).json
+            payload.homeId.push(homeId)
+            this.create(identity, payload)
+        },
         create: async (identity, payload) => {
             try {
                 return unWrap(await fetch(`https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/users/${identity.id}`, {
