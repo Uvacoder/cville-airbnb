daa2eccc227541521eb35252aa28726072b8d232
Josh Deltener <hecktarzuli@gmail.com>
2021-05-05 20:32:23 -0500

    85 property availability - search page

--- pages/search.vue
+++ pages/search.vue
@@ -43,7 +43,7 @@ export default {
     }
   },
   async beforeRouteUpdate(to, from, next){
-      const data = await this.$dataApi.getHomesByLocation(to.query.lat, to.query.lng)
+      const data = await this.$dataApi.getHomesByLocation(to.query.lat, to.query.lng, to.query.start, to.query.end)
       this.homes = data.json.hits
       this.label = to.query.label
       this.lat = to.query.lat
@@ -53,7 +53,7 @@ export default {
   },
 
   async asyncData({ query, $dataApi }){
-    const data = await $dataApi.getHomesByLocation(query.lat, query.lng)
+    const data = await $dataApi.getHomesByLocation(query.lat, query.lng, query.start, query.end)
     return {
       homes: data.json.hits,
       label: query.label,
--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -52,8 +52,12 @@ export default function({ $config }, inject){
         }
     }
 
-    async function getHomesByLocation(lat, lng, radiusInMeters = 1500 * 15){
+    async function getHomesByLocation(lat, lng, start, end, radiusInMeters = 1500 * 15){
         try {
+            const days = []
+            for(var day = start;day <= end; day += 86400){
+                days.push(`availability:${day}`)
+            }
             return unWrap(await fetch(`https://${$config.algolia.appId}-dsn.algolia.net/1/indexes/homes/query`, {
                 headers,
                 method: 'POST',
@@ -61,6 +65,7 @@ export default function({ $config }, inject){
                     aroundLatLng: `${lat},${lng}`,
                     aroundRadius: radiusInMeters,
                     hitsPerPage: 10,
+                    filters: days.join(' AND '),
                     attributesToHighlight: [],
                 })
             }))
