f9fc53f243363defe609acbe830cb333842fd8e7
Josh Deltener <hecktarzuli@gmail.com>
2021-05-19 19:55:48 -0500

    89 - creating stripe proxy apis part 2

--- modules/stripe/index.js
+++ modules/stripe/index.js
@@ -1,12 +1,13 @@
 import stripeLib from 'stripe'
 import getApis from './apis'
-import { rejectHitBadRequest } from '../helpers'
+import { rejectHitBadRequest, sendJSON } from '../helpers'
 
 export default function(){
     const algoliaConfig = this.options.privateRuntimeConfig.algolia
     const apis = getApis(algoliaConfig)
     const secretKey = this.options.privateRuntimeConfig.stripe.secretKey
     const stripe = stripeLib(secretKey)
+    const cloudName = this.options.cloudinary.cloudName
 
     this.nuxt.hook('render:setupMiddleware', (app) => { 
         app.use('/api/stripe/create-session', createSession)
@@ -17,5 +18,27 @@ export default function(){
         if(!body || !body.homeId || !body.start || !body.end || !body.start >= body.end){
             return rejectHitBadRequest(res)
         }
+
+        const home = (await apis.homes.get(body.homeId)).json
+        const nights = (body.end - body.start) / 86400
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types:['card'],
+            mode: 'payment',
+            success_url: `http://localhost:3000/home/${body.homeId}?result=success`,
+            cancel_url: `http://localhost:3000/home/${body.homeId}`,
+            line_items:[{
+                quantity: 1,
+                price_data: {
+                    currency: 'USD',
+                    unit_amount: home.pricePerNight * nights * 100,
+                    product_data:{
+                        name: 'Reservation for ' + home.title,
+                        images:[`https://res.cloudinary.com/${cloudName}/image/upload/${home.images[0]}`]
+                    }
+                },
+            }]
+        })
+
+        sendJSON({ id: session.id }, res)
     }
 }
