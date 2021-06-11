c2e68d8ba45e95f710a5c27793380a8d5c570696
Josh Deltener <hecktarzuli@gmail.com>
2021-06-10 20:36:33 -0500

    94 - stripe webhooks

--- modules/stripe/index.js
+++ modules/stripe/index.js
@@ -13,6 +13,14 @@ export default function(){
     this.nuxt.hook('render:setupMiddleware', (app) => { 
         app.use('/api/stripe/create-session', createSession)
     })
+    
+    this.nuxt.hook('render:setupMiddleware', (app) => { 
+        app.use('/hooks/stripe', (req, res, next) => {
+            const meta = req.body.data.object.metadata
+            res.end(`${meta.identityId} booked ${meta.homeId}!!!!`)
+        })
+    })
+
 
     async function createSession(req, res){
         const body = req.body 
@@ -23,6 +31,12 @@ export default function(){
         const home = (await apis.homes.get(body.homeId)).json
         const nights = (body.end - body.start) / 86400
         const session = await stripe.checkout.sessions.create({
+            metadata:{
+                identityId: req.identity.id,
+                homeId: body.homeId,
+                start: body.start,
+                end: body.end,
+            },
             payment_method_types:['card'],
             mode: 'payment',
             success_url: `${rootUrl}/home/${body.homeId}?result=success`,
