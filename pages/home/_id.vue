9a8bc6f292857e9fef9b1f05b96137ac29439dae
Josh Deltener <hecktarzuli@gmail.com>
2021-06-20 23:05:50 -0500

    96 - working with microdata

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -6,10 +6,33 @@
     <PropertyMap :home="home"/>
     <PropertyReviews :reviews="reviews"/>
     <PropertyHost :user="user"/>    
+    <script type="application/ld+json" v-html="getSchema"></script>
 </div>
 </template>
 <script>
 export default {
+    computed:{
+        getSchema(){
+            return JSON.stringify({
+                "@context": "http://schema.org",
+                "@type": "BedAndBreakfast",
+                "name": this.home.title,
+                "image":this.$img(this.home.images[0], {width:1200}, { provider: 'cloudinary'}),
+                "address":{
+                    "@type": "PostalAddress",
+                    "addressLocality": this.home.location.city,
+                    "addressRegion": this.home.location.state,
+                    "postalCode": this.home.location.zipcode,
+                    "streetAddress": this.home.location.address,
+                },
+                "aggregateRating":{
+                    "@type": "AggregateRating",
+                    "ratingValue": this.home.reviewValue,
+                    "reviewCount": this.home.reviewCount,
+                }
+            })
+        }
+    },
     head(){
         return {
             title: this.home.title,    
