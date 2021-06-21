9afaecf6a746437eea274d038347368199206fc5
Josh Deltener <hecktarzuli@gmail.com>
2021-06-20 22:46:35 -0500

    95 - social cards - twitter

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -19,7 +19,8 @@ export default {
                 { hid: 'og-desc', property: 'og:description', content: this.home.description },
                 { hid: 'og-image', property: 'og:image', 
                   content: this.$img(this.home.images[0], {width:1200}, { provider: 'cloudinary'})},
-                { hid: 'og-url', property: 'og:url', content: `${this.$config.rootUrl}/home/${this.home.objectID}`}
+                { hid: 'og-url', property: 'og:url', content: `${this.$config.rootUrl}/home/${this.home.objectID}`},
+                { hid: 't-type', name: 'twitter:card', content: 'summary_large_image' }
             ]               
         }
     },   
